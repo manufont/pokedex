84c18822cff39397053ba7dcd89e983e79511d85
Emmanuel Fonteneau <emmanuel.fonteneau@eisti.fr>
2017-11-29 15:53:31 +0100

    Remove redux devtools store enhancer

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import promise from "redux-promise-middleware";
 
@@ -6,12 +6,6 @@ import reducer from "./reducer";
 
 const middlewares = [thunk, promise()];
 
-const store = createStore(
-  reducer,
-  compose(
-    applyMiddleware(...middlewares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(reducer, applyMiddleware(...middlewares));
 
 export default store;
